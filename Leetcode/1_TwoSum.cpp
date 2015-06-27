4b7e1265b257da947a88dd9f34a781d5211e9831
RongLi <rongrong.118@hotmail.com>
2015-06-26 17:28:19 -0700

    update comment

--- Leetcode/1_TwoSum.cpp
+++ Leetcode/1_TwoSum.cpp
@@ -19,10 +19,12 @@ Output: index1=1, index2=2
 using namespace std;
 
 /*
-keep a hashmap, number value (target - number value) as key and index as value
+keep a hashmap, number value as key and number index as its value
 travese array if we cannot find key in hasmap(for now no number can match sum with it), 
 insert it.
 otherwise, put its index and item's value in hashmap in output array
+
+running time:O(n).
 */ 
 
 
@@ -31,22 +33,20 @@ class Solution
 {
 public:
 	vector<int> twoSum(vector<int> &numbers, int target){
-		vector<int> output;
-		//vector<int>::iterator itr;
-		unordered_map<int,int> myMap;  //<key, value>: value,index
+		vector<int> output;  //output vector
+		unordered_map<int,int> myMap;  //<key, value>: keep value and index
 		int findValue;
-		//map<int,int>::iterator mitr;
-		int vectorLength=numbers.size();
-		for(int i=0;i<vectorLength;i++){
-			findValue=target-(numbers[i]);
-			if(myMap.find(findValue)==myMap.end()){  //what is stored in map.end()??
-				myMap[numbers[i]]=i;
+
+		for(int i=0;i<numbers.size();i++){
+			findValue=target-(numbers[i]);   
+			if(myMap.find(findValue)==myMap.end()){  //not find 
+				myMap[numbers[i]]=i;      		     //put in hashmap
 			}else{
-				output.push_back(myMap[findValue]+1);
+				output.push_back(myMap[findValue]+1); //save in output vector
 				output.push_back(i+1);								
 			}
 		}
-		//sort(output.begin(),output.end());
+
 		return output;
 	}
 };
