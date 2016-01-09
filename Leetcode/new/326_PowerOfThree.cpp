c31435b554b0e3ea47f0fe5205ce35952647f389
RongLi <rongrong.118@hotmail.com>
2016-01-08 18:22:16 -0800

    change format

--- Leetcode/new/326_PowerOfThree.cpp
+++ Leetcode/new/326_PowerOfThree.cpp
@@ -1,5 +1,5 @@
 /*
-326. Power of Three 
+326. Power of Three
 Total Accepted: 2159 Total Submissions: 5961 Difficulty: Easy
 Given an integer, write a function to determine if it is a power of three.
 
@@ -11,7 +11,6 @@ Special thanks to @dietpepsi for adding this problem and creating all test cases
 
 Subscribe to see which companies asked this questio
 */
-
 
 /*
 if a number is power of 3, it could be divided by 1162261467 since 1162261467 is largest
@@ -28,15 +27,15 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         //corner case
-        if(n<=0) return false;
-    
-        if(1162261467%n==0)
+        if (n <= 0) return false;
+
+        if (1162261467 % n == 0)
             return true;
         return false;
     }
 };
 
-int main(){
+int main() {
 
     return 0;
 }
