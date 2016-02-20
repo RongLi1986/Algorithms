0a4919d7ecc2eca87aa4cfa6513b9dfa1520ac04
RongLi <rongrong.118@hotmail.com>
2016-02-19 16:55:37 -0800

    added comment in 316

--- Leetcode/new/316_removeDuplicatedLetters.cpp
+++ Leetcode/new/316_removeDuplicatedLetters.cpp
@@ -11,22 +11,17 @@ Given "cbacdcbc"
 Return "acdb"
 */
 
+
 /*
-use stack.
+using stack.
 
-first scan count number of each unique letter in string (use map)
+first scan string and count times of each letter in string (use map)
 
-then scan string again. for each letter, if it is not in stack, compare it with stack top's letter.
+then scan string again. for each letter, if it is not in stack, begin a loop which compare it with stack top's letter.
 
-if top letter cnt is not 0 and larget than it, pop stack and push it.
- 
-用栈。
+if top letter count is not 0 (which means it will appear) and larger than current letter, pop stack top letter.
 
-首先对字符串出现的个数进行统计。
-
-然后对字符串扫描，每遇到一个字符串，判断其是否在栈中，如果在则跳过。（计数 – 1）
-
-如果不在栈中，和栈顶的元素判断，要是当前栈顶的元素比较大而且cnt不为0（也就是说之后还有这个元素），就把栈顶弹出。然后把当前的元素入栈。
+when loop overand push it.
 */
 
 #include <iostream>
@@ -40,28 +35,30 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
     	unordered_map<char,int> hashMap;
-    	unordered_map<char,int> visitMap; //letter in stk
+    	unordered_map<char,int> visitMap; //letter in or not in stk
     	stack<char> stk;
-    	//count letter's numbers
+    	
+        //count letter's appear times
     	for(int i=0;i<s.size();i++){
     		if(hashMap.find(s[i])==hashMap.end()){
     			hashMap[s[i]]=1;
     		}else{
     			hashMap[s[i]]++;
     		}
-    		visitMap[s[i]]=0;
+    		visitMap[s[i]]=0;  //initial not in stk
     	}
 
+        //
     	for(int i=0;i<s.size();i++){
     		if(visitMap[s[i]]==0){
-    			if(stk.empty()){
+    			if(stk.empty()){ //first one
     				stk.push(s[i]);
     				visitMap[s[i]]=1;
     			}else{
     				while(stk.top()>s[i] && hashMap[stk.top()]>0 ){
     					visitMap[stk.top()]=0;
     					stk.pop();
-    					if(stk.empty()) break;
+    					if(stk.empty()) break;  //corner case stk become empty
     				}
     				
     				stk.push(s[i]);
@@ -75,6 +72,7 @@ public:
 
     	string res;
 
+        //res is reverse stk;
     	while(!stk.empty()){
     		res=stk.top()+res;
     		stk.pop();
