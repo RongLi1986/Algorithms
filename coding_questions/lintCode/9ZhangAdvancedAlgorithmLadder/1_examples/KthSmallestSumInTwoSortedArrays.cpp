b7b1655e39f2db2b619b1f7c817f14f456244766
rongli <rongli.work@gmail.com>
2019-09-11 20:45:41 -0400

    updated questions in chapter 1

--- coding_questions/lintCode/9ZhangAdvancedAlgorithmLadder/1_examples/KthSmallestSumInTwoSortedArrays.cpp
+++ coding_questions/lintCode/9ZhangAdvancedAlgorithmLadder/1_examples/KthSmallestSumInTwoSortedArrays.cpp
@@ -1,37 +1,20 @@
 /*******************************************************
 Description
-中文
-English
-Find the kth smallest number in a row and column sorted matrix.
+在两个排序数组中找到第K大元素的和
 
-Each row and each column of the matrix is incremental.
+例子
+input:
+[4,7,8]
+[1,2]
+2
 
-Example
-Example 1:
+return (2+7) = 9
 
-Input:
-[
-  [1 ,5 ,7],
-  [3 ,7 ,8],
-  [4 ,8 ,9],
-]
-k = 4
-Output: 5
-Example 2:
-
-Input:
-[
-  [1, 2],
-  [3, 4]
-]
-k = 3
-Output: 3
 ********************************************************/
 
-
 /*
-解题思路：
-O(klogN)
+similar to KthSmallestNumberInSortedMatrix
+
 */ 
 
 class Solution {
@@ -41,21 +24,20 @@ public:
      * @param k: An integer
      * @return: the kth smallest number in the matrix
      */
-    int kthSmallest(vector<vector<int>> &matrix, int k) {
+    int kthSmallest(vector<int> &a, vector<int> &b, int k) {
         // write your code here
-        if(matrix.empty() || matrix[0].empty()) return -1;
-        if(matrix.size() * matrix[0].size() < k) return -1;
         
         //define priority_queue with custom comparator
+        //pair<int, pair<int, int>> ==> (value, (x,y));
         auto comp = [] (pair<int, pair<int, int>> &a, pair<int, pair<int, int>> &b) -> bool { return a.first > b.first; };
         priority_queue<pair<int, pair<int, int> >,
                 vector<pair<int, pair<int, int> >>, 
                 decltype(comp) > pq (comp);
         
         //visitor map
-        vector<vector<int>> vistor_map(matrix.size(), vector<int>(matrix[0].size(), 0)); 
+        vector<vector<int>> vistor_map(a.size(), vector<int>(b.size(), 0)); 
         
-        pq.push({matrix[0][0], {0,0}});
+        pq.push({a[0] + b[0], {0, 0}});
         vistor_map[0][0] = 1;
         
         //pop k-1 item
@@ -65,12 +47,13 @@ public:
             int x = item.second.first;
             int y = item.second.second;
             //cout<<matrix[x][y]<<endl;
-            if(x + 1 >= 0 && x + 1 < matrix.size() && vistor_map[x + 1][y] == 0){
-                pq.push({matrix[x + 1][y], {x + 1,y}});
+            if(x + 1 >= 0 && x + 1 < a.size() && vistor_map[x + 1][y] == 0){
+                pq.push({a[x + 1] + b[y], {x + 1, y}});
                 vistor_map[x + 1][y] = 1;
             }
-            if(y + 1 >= 0 && y + 1 < matrix[0].size() && vistor_map[x][y + 1] == 0){
-                pq.push({matrix[x][y + 1], {x,y + 1}});
+
+            if(y + 1 >= 0 && y + 1 < b.size() && vistor_map[x][y + 1] == 0){
+                pq.push({a[x] + b[y + 1], {x, y + 1}});
                 vistor_map[x][y + 1] = 1;
             }
         }
